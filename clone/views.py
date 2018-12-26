c444692c84cc6fcb474fd8e1c7d8e370e63e73bf
nignanthomas <nignanthomas@gmail.com>
2018-12-26 19:36:24 +0300

    create navbar; create index view

--- clone/views.py
+++ clone/views.py
@@ -2,41 +2,23 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Project,Profile,
-# from .forms import NewPostForm,ProfileForm,CommentForm
+from .models import Profile,Project,Rating
 # from django.http import JsonResponse
 # import json
 from django.db.models import Q
 
 # Create your views here.
-def timeline(request):
-    posts= Post.objects.all().order_by("-id")
-    profiles= Profile.objects.all()
-    current_user = request.user
 
-    comments=Comment.objects.all()
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            post_id = int(request.POST.get("idpost"))
-            post = Post.objects.get(id = post_id)
-            comment = form.save(commit=False)
-            comment.username = request.user
-            comment.post = post
-            comment.save()
-        return redirect('timeline')
-
-    else:
-        form = CommentForm()
-
-    return render(request,'timeline.html',{"posts":posts,"profiles":profiles,"current_user":current_user,"comments":comments,"form":form,})
+def index(request):
+    user = request.user
+    return render(request,'index.html',{"user":user})
 
 
 @login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'search' in request.GET and request.GET["search"]:
         search_term = request.GET.get("search")
-        searched_users = Profile.search_profile(search_term)
+        searched = Project.filter_by_search_term(search_term)
         message=f"Search results for: {search_term}"
 
         return render(request,'search.html',{"message":message,"users":searched_users})
@@ -46,13 +28,6 @@ def search_results(request):
         return render(request,'search.html',{"message":message})
 
 
-@login_required(login_url='/accounts/login/')
-def explore(request):
-    posts = Post.objects.all()
-    profiles= Profile.objects.all()[:3]
-    # form=CommentForm()
-    # comments=Comment.objects.all()
-    return render(request,"explore.html",{"posts":posts,"profiles":profiles,})
 
 
 @login_required(login_url='/accounts/login/')
@@ -60,8 +35,8 @@ def profile(request,id):
     user_object = request.user
     current_user = Profile.objects.get(username__id=request.user.id)
     user = Profile.objects.get(username__id=id)
-    posts = Post.objects.filter(upload_by = user)
-    return render(request, "profile.html", {"current_user":current_user,"posts":posts,"user":user,"user_object":user_object,})
+    projects = Project.objects.filter(upload_by = user)
+    return render(request, "profile.html", {"current_user":current_user,"projects":projects,"user":user,"user_object":user_object,})
 
 @login_required(login_url='/accounts/login/')
 def new_post(request):
